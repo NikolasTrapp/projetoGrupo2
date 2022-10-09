99b97c0ade373a7ec4dab5915f5332a4563e602a
Nikolas Trapp <nikolastrapp@gmail.com>
2022-10-09 01:01:50 -0300

    Adicionando sistemas de filtro e busca

--- js/db.js
+++ js/db.js
@@ -1,11 +1,66 @@
 carros = [
     {
-        nome: "Nome do carro",
-        modelo: "Modelo do carro",
-        marca: "Marca do carro",
-        preco: "Preco",
-        nome_imagem: "nome da imagem",
-        ano: "Ano do carro",
-        quilometragem: "km dos carro"
-    }
+        modelo: "Fusca",
+        marca: "Volkswagen",
+        preco: 5000.90,
+        nome_imagem: "fusca.jpg",
+        ano: 2012,
+        quilometragem: 1200
+    },
+    {
+        modelo: "Cleber",
+        marca: "Volkswagen",
+        preco: 6000.90,
+        nome_imagem: "fusca.jpg",
+        ano: 2013,
+        quilometragem: 1200
+    },
+    {
+        modelo: "Santana",
+        marca: "Volkswagen",
+        preco: 7000.90,
+        nome_imagem: "fusca.jpg",
+        ano: 2014,
+        quilometragem: 1200
+    },
+    {
+        modelo: "Fusca",
+        marca: "Volkswagen",
+        preco: 8000.90,
+        nome_imagem: "fusca.jpg",
+        ano: 2015,
+        quilometragem: 1200
+    },
+    {
+        modelo: "Fusca",
+        marca: "Fiat",
+        preco: 9000.90,
+        nome_imagem: "fusca.jpg",
+        ano: 2016,
+        quilometragem: 1200
+    },
+    {
+        modelo: "Fusca",
+        marca: "Ford",
+        preco: 10000.90,
+        nome_imagem: "fusca.jpg",
+        ano: 2017,
+        quilometragem: 1200
+    },
+    {
+        modelo: "Fusca",
+        marca: "Ford",
+        preco: 18900.90,
+        nome_imagem: "fusca.jpg",
+        ano: 2018,
+        quilometragem: 1200
+    },
+    {
+        modelo: "Cleber",
+        marca: "Volkswagen",
+        preco: 18900.90,
+        nome_imagem: "fusca.jpg",
+        ano: 2012,
+        quilometragem: 1200
+    },
 ];
---
+++ js/estoque.js
@@ -0,0 +1,131 @@
+const estoque = document.getElementById("estoque");
+
+function gerarEstrutura(carros){
+    let estrutura = "";
+    carros.map(carro => {
+        estrutura += `
+        <div class="card" style="width: 18rem;">
+            <img src="img/${carro.nome_imagem}" class="card-img-top" alt="${carro.nome_imagem}" style="width: 100%; height: 200px;>
+            <div class="card-body">
+                <h5 class="card-title">${carro.modelo}</h5>
+                <h5 class="card-title">${carro.marca}</h5>
+                <p class="card-text">R$ ${parseFloat(carro.preco).toFixed(2)}</p>
+                <p class="card-text">${carro.quilometragem} km</p>
+                <a href="#" class="btn btn-primary">Tenho interesse</a>
+            </div>
+        </div>
+        `;
+    });
+    return estrutura;
+}
+
+function carregarVeiculos() {
+    estoque.innerHTML = gerarEstrutura(carros);
+}
+
+function carregarValores(){
+    const marcas = document.querySelector("#marcas-container");
+    const modelos = document.querySelector("#modelos-container");
+    let marcas_carro = new Set(carros.map(carro => carro.marca));
+    let modelos_carro = new Set(carros.map(carro => carro.modelo));
+    
+    marcas_carro.forEach(marca => {
+        marcas.innerHTML += `<label class="d-block"><input class="marcas" type="checkbox" value="${marca}"> ${marca}</label>`;
+    });
+    modelos_carro.forEach(modelo => {
+        modelos.innerHTML += `<label class="d-block"><input class="modelos" type="checkbox" value="${modelo}"> ${modelo}</label>`;
+    });
+}
+
+document.querySelector("#pesquisa").addEventListener("click", function(){
+    const pesquisa = document.getElementById("input-pesquisa").value;
+
+    const lista_carros = carros.filter(function(carro){
+        return carro.modelo == pesquisa || carro.marca == pesquisa;
+    }) 
+    
+    if (lista_carros.length > 0){
+        estoque.innerHTML = gerarEstrutura(lista_carros);
+    } else {
+        alert("Não encontramos nenhum veículo com esta marca ou modelo!");
+    }
+});
+
+document.querySelector("#limpar-busca").addEventListener("click", function(){
+    estoque.innerHTML = gerarEstrutura(carros);
+});
+
+document.querySelector("#aplicar-filtros").addEventListener("click", function(){
+    let lista_marcas = [];
+    let lista_modelos = [];
+    let anoMin = document.getElementById("ano-minimo").value;
+    let anoMax = document.getElementById("ano-maximo").value;
+    let precoMin = document.getElementById("preco-minimo").value;
+    let precoMax = document.getElementById("preco-maximo").value;
+
+
+    for (let marca of document.querySelectorAll(".marcas")){
+        marca.checked ? lista_marcas.push(marca.value) : null;
+    }
+    for (let modelo of document.querySelectorAll(".modelos")){
+        modelo.checked ? lista_modelos.push(modelo.value) : null;
+    }
+    
+    let filtro = carros.filter(function(carro){
+        if (lista_marcas.length > 0){
+            return lista_marcas.includes(carro.marca.trim());
+        } else {
+            return true;
+        }
+    }).filter(function(carro){
+        if (lista_modelos.length > 0){
+            return lista_modelos.includes(carro.modelo.trim());
+        } else {
+            return true;
+        }
+    }).filter(function(carro){
+        if (anoMin || anoMax){
+            if (anoMin && !anoMax){
+                return carro.ano >= anoMin;
+            } else if (!anoMin && anoMax){
+                return carro.ano <= anoMax;
+            } else {
+                return carro.ano >= anoMin && carro.ano <= anoMax;
+            }
+        } else {
+            return true;
+        }
+    }).filter(function(carro){
+        if (precoMin || precoMax){
+            if (precoMin && !precoMax){
+                return carro.preco >= precoMin;
+            } else if (!precoMin && precoMax){
+                return carro.preco <= precoMax;
+            } else {
+                return carro.preco >= precoMin && carro.preco <= precoMax;
+            }
+        } else {
+            return true;
+        }
+    });
+
+    if (filtro.length > 0){
+        estoque.innerHTML = gerarEstrutura(filtro);
+    } else {
+        alert("Nenhum carro encontrado")
+    }
+});
+
+document.querySelector("#limpar-filtros").addEventListener("click", function(){
+    document.querySelectorAll(".marcas").forEach(input => input.checked = false);
+    document.querySelectorAll(".modelos").forEach(input => input.checked = false);
+    document.getElementById("ano-minimo").value = "";
+    document.getElementById("ano-maximo").value = "";
+    document.getElementById("preco-minimo").value = "";
+    document.getElementById("preco-maximo").value = "";
+    console.log(carros);
+    estoque.innerHTML = gerarEstrutura(carros);
+});
+
+carregarVeiculos();
+carregarValores();
