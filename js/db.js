3910d4fecd0974ea161d380397542e61bce01e00
Nikolas Trapp <nikolastrapp@gmail.com>
2022-10-08 22:23:52 -0300

    First commit

---
+++ js/db.js
@@ -0,0 +1,11 @@
+carros = [
+    {
+        nome: "Nome do carro",
+        modelo: "Modelo do carro",
+        marca: "Marca do carro",
+        preco: "Preco",
+        nome_imagem: "nome da imagem",
+        ano: "Ano do carro",
+        quilometragem: "km dos carro"
+    }
+];
